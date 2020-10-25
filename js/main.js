9c306b6d861a651eac04a6ba93046fc5e188fab4
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-10-25 00:52:44 -0400

    new data

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function getData(map){
 
 
 //part that gets the school data
- $.ajax("data/details_true.geojson",{
+ $.ajax("data/incidents_true.geojson",{
 	dataType: "json",
 	 success: function(response){
 		 var datapoints = response.features;
@@ -45,7 +45,6 @@ function getData(map){
 
 		//function for popup
 		function buildPopupContent(datapoints,feature){
-			name = "Incident:";
 			// I have this stuff commented out because your data is ... XML? 
 			// But if the features were formatted as GeoJSON, you could use this syntax to grab properties
 			street = feature.feature.properties.STREET;
@@ -72,6 +71,48 @@ function getData(map){
 //bracket that closes out the async call function. Don't erase!
 });
 
+//part that gets the school data
+$.ajax("data/matching_details_subset_TRUE.geojson",{
+	dataType: "json",
+	 success: function(response){
+		 var datapoints = response.features;
+
+		 var incidentsMarkerOptions = {
+			radius : 4,
+			fillColor : "#FFFF00",
+			color : "000",
+			weight : 1, 
+			opacity : 1,
+			fillOpacity : .8
+		};
+
+		//function for popup
+		function buildPopupContent(datapoints,feature){
+			// I have this stuff commented out because your data is ... XML? 
+			// But if the features were formatted as GeoJSON, you could use this syntax to grab properties
+			street = feature.feature.properties.address;
+			offense = feature.feature.properties.OFFENSE_DESCRIPTION;
+			when = feature.feature.properties.OCCURRED_ON_DATE;
+			content = "<strong>Incident: </strong>" + offense + "<br>" + "<strong>Time and date: </strong>" + when + "<br>" + "<strong>Street: </strong>" + street;
+			//content = "<strong>Name: </strong>" + name + "<br>" + "<strong>Address: </strong>" + address + "<br>" + "<strong>School type: </strong>" + type;
+			feature.bindPopup(content);
+		}
+
+		//add geojson layer to map w/ unique symbology
+		var incidentsLayer = L.geoJSON(datapoints, {
+			onEachFeature: buildPopupContent,
+			pointToLayer : function (feature, latlng) {
+				return L.circleMarker(latlng, incidentsMarkerOptions);
+			}
+		}).addTo(map);
+
+
+// THREE IMPORTANT CLOSING BRACKETS AT THE END OF GETDATA() FUNCTION!
+//bracket that closes out the async response. Don't erase!
+}
+
+//bracket that closes out the async call function. Don't erase!
+});
 
 //bracket that closes out the getData() function. Don't erase!
 };
