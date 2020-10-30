0f2be8fb3bdb263f369109d685f12123f01fe5b3
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-10-29 22:14:00 -0400

    point layers

--- js/main-choro-alt.js
+++ js/main-choro-alt.js
@@ -1,12 +1,31 @@
 var tracts = $.ajax({
     url: "data/Incidents_True_CensusTracts.geojson",
     dataType: "json",
-    success: console.log("data successfully loaded."),
+    success: console.log("tracts successfully loaded."),
     error: function(xhr) {
         alert(xhr.statusText)
     }
-})
-$.when(tracts).done(function() {
+});
+
+var incidentPoints = $.ajax({
+    url: "data/incidents_true.geojson",
+    dataType: "json",
+    success: console.log("incident points successfully loaded."),
+    error: function(xhr) {
+        alert(xhr.statusText)
+    }
+});
+
+var detailPoints = $.ajax({
+    url: "data/matching_details_subset.geojson",
+    dataType: "json",
+    success: console.log("details points successfully loaded."),
+    error: function(xhr) {
+        alert(xhr.statusText)
+    }
+});
+
+$.when(tracts, incidentPoints, detailPoints).done(function() {
     var map = L.map('map',{
 		center: [42.350,-71.065],
 		zoom: 14,
@@ -20,6 +39,12 @@ $.when(tracts).done(function() {
 		maxZoom: 21,
 		ext: 'png'
 }).addTo(map);
-    // Add requested external GeoJSON to map
-    var incidentsTracts = L.geoJSON(tracts.responseJSON).addTo(map);
+ 
+// Add requested external GeoJSON to map
+    var tractsLayer = L.geoJSON(tracts.responseJSON).addTo(map);
+
+    var incidentsLayer = L.geoJSON(incidentPoints.responseJSON).addTo(map);
+
+    var detailLayer = L.geoJSON(detailPoints.responseJSON).addTo(map);
+
 });
